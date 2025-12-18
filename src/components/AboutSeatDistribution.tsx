73b3b9ea4bc29434c96de1ac5470daa0d810be12
restzeteldans <florisloeffler@hotmail.nl>
2025-12-18 10:54:19 +0100

    V 1.0 - Added the video
    Added the video. The website is now in its completed state for V1.0

--- src/components/AboutSeatDistribution.tsx
+++ src/components/AboutSeatDistribution.tsx
@@ -33,7 +33,7 @@ export function AboutSeatDistribution() {
           <iframe
             width="100%"
             height="100%"
-            src="https://www.youtube.com/embed/7bNKPIRPaQA"
+            src="https://www.youtube.com/embed/EvyrDy8Gwjg"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
